a6cbfc3770d293bca6e0ac19f535e0bca3632dfb
MaxLHy0424 <MaxLHy974413@outlook.com>
2024-02-15 07:35:13 +0800

    回退更改

--- main.cpp
+++ main.cpp
@@ -49,7 +49,7 @@ void Configuration(bool ReConfigure){
 void About(){
     printf("<关于>\n\n");
     printf("   [软件名称] 机房控制软件克星 (Computer Room Control Software Nemesis)\n");
-    printf("   [版 本 号] v3.1.0 (Dev3)\n");
+    printf("   [构建版本] Dev 31004\n");
     printf("   [软件作者] MaxLHy0424\n");
     printf("   [主 仓 库] https://github.com/MaxLHy0424/Computer-Room-Control-Software-Nemesis\n\n");
     printf("   (C) 2023-2024 MaxLHy0424, All Rights Reserved.\n\n");
