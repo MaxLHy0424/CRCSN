403fd2935e9f5023d9e6daf7beb85889ab542622
MaxLHy0424 <MaxLHy974413@outlook.com>
2024-02-16 12:10:39 +0800

    调整 goto 标签

--- main.cpp
+++ main.cpp
@@ -19,7 +19,7 @@ void Configuration(bool reConfig){
         }
         system("Pause");
         system("cls");
-        goto SkipConfiguration;
+        goto Skip;
     }
     {
         std::string configItem[4];
@@ -42,14 +42,14 @@ void Configuration(bool reConfig){
         }
     }
     fIn.close();
-    SkipConfiguration:
+    Skip:
     system("cls");
     Start();
 }
 void About(){
     printf("<关于>\n\n");
     printf("   [软件名称] 机房控制软件克星 (Computer Room Control Software Nemesis)\n");
-    printf("   [构建版本] Dev 31008\n");
+    printf("   [构建版本] Dev 31081\n");
     printf("   [软件作者] MaxLHy0424\n");
     printf("   [主 仓 库] https://github.com/MaxLHy0424/Computer-Room-Control-Software-Nemesis\n\n");
     printf("   (C) Copyright 2023-2024 MaxLHy0424, All Rights Reserved.\n\n");
