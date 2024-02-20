6d94ed20991ea8f340019b7ebe48871a1e57050f
MaxLHy0424 <MaxLHy974413@outlook.com>
2024-02-20 16:20:59 +0800

    更新配置文件及对应代码

--- main.cpp
+++ main.cpp
@@ -31,11 +31,10 @@ bool Configuration(bool reConfig){
         system(configItem[0].c_str());
         configItem[1]="Title "+configItem[1];
         system(configItem[1].c_str());
-        if(configItem[3]=="true"){
-            system("Set Path=%path%;\"C:\\Windows\\System32\\\"");
-            system("Set Path=%path%;\"C:\\Windows\\SysWOW64\\\"");
+        if(configItem[2]!="$SKIP$"){
+            system(configItem[2].c_str());
         }
-        if(reConfig&&configItem[2]=="true"){
+        if(reConfig&&configItem[3]=="1"){
             printf("[提示] 重载配置完成!\n\n");
             printf("########################################\n\n");
             printf("按任意键返回主菜单.\n\n");
@@ -51,7 +50,7 @@ bool Configuration(bool reConfig){
 void About(){
     printf("| 主菜单 > 关于 |\n\n");
     printf("    [软件名称] 机房控制软件克星 (Computer Room Control Software Nemesis)\n");
-    printf("    [构建版本] Dev 31084\n");
+    printf("    [构建版本] Dev 31085\n");
     printf("    [软件作者] MaxLHy0424\n");
     printf("    [主 仓 库] https://github.com/MaxLHy0424/Computer-Room-Control-Software-Nemesis\n\n");
     printf("    (C) Copyright 2023-2024 MaxLHy0424, all rights reserved.\n\n");
