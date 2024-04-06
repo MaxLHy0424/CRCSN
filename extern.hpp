af614a1432cf5073edca8bb19f26467d656e8a9a
MaxLHy0424 <MaxLHy974413@outlook.com>
2024-04-06 15:09:05 +0800

    修改缩进

--- extern.hpp
+++ extern.hpp
@@ -5,13 +5,13 @@
 char Code[3]{};
 std::string CfgDat[3]{"\0","NULL","\0"};
 DWORD KernalVersion(){
-	DWORD version{GetVersion()};
-	DWORD major{(DWORD)(LOBYTE(LOWORD(version)))};
-	DWORD minor{(DWORD)(HIBYTE(LOWORD(version)))};
+    DWORD version{GetVersion()};
+    DWORD major{(DWORD)(LOBYTE(LOWORD(version)))};
+    DWORD minor{(DWORD)(HIBYTE(LOWORD(version)))};
     return major*10U+minor;
 }
 BOOL PauseOp(){
-	printf("是否继续 (Y/N): ");
+    printf("是否继续 (Y/N): ");
     for(;;){
         scanf("%s",&Code[1]);
         if((Code[1]=='Y'||Code[1]=='y'||Code[1]=='N'||Code[1]=='n')&&Code[2]==0){
@@ -20,7 +20,7 @@ BOOL PauseOp(){
             printf("输入错误, 请重新输入: ");
         }
     }
-	if(Code[1]=='Y'||Code[1]=='y'){
+    if(Code[1]=='Y'||Code[1]=='y'){
         return TRUE;
     }
     return FALSE;
