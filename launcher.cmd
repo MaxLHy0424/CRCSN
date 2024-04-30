953755359c0eff1df93c1c625834fbd70b28d1ec
MaxLHy0424 <MaxLHy974413@outlook.com>
2024-04-30 19:18:54 +0800

    修改默认启动配置

--- launcher.cmd
+++ launcher.cmd
@@ -3,7 +3,6 @@ SetLocal EnableDelayedExpansion
 Title CRCSN Launcher
 PUSHD %~DP0 & CD /D "%~dp0"
 %1 %2
-: 如果需要以管理员权限运行, 请将下一行的前的": "去掉.
 : MsHTA Vbscript:CreateObject("Shell.Application").ShellExecute("%~S0","goto :RunAs","","RunAs",1)(Window.Close)&Goto:EOF
 :RunAs
-.\bin\main.exe -color=9 -beta
+.\bin\main.exe -color=9
