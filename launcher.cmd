37a6154af79bb67bad1665cdac62abaaa975d4df
MaxLHy0424 <MaxLHy974413@outlook.com>
2024-05-14 18:04:40 +0800

    回退启动脚本名称

--- launcher.cmd
+++ launcher.cmd
@@ -1,7 +1,7 @@
 @Echo Off
 SetLocal EnableDelayedExpansion
 Title CRCSN Launcher
-PUSHD %~DP0 & CD /D "%~dp0"
+PushD %~DP0 & CD /D "%~dp0"
 %1 %2
 : MsHta Vbscript:CreateObject("Shell.Application").ShellExecute("%~S0","Goto :RunAs","","RunAs",1)(Window.Close)&Goto:EOF
 :RunAs
