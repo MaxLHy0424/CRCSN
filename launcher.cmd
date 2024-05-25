b24bf0c9ffdb191055d42d82f107ea5092f98f5a
MaxLHy0424 <MaxLHy974413@outlook.com>
2024-05-25 17:27:21 +0800

    修改命令执行顺序

--- launcher.cmd
+++ launcher.cmd
@@ -1,10 +1,10 @@
 @Echo Off
 Title CRCSN Launcher
-Echo Launcher v1.0.8
-SetLocal EnableDelayedExpansion
-PushD %~DP0 & CD /D "%~DP0"
-%1 %2
-: MsHta VbScript:CreateObject("Shell.Application").ShellExecute("%~S0","GoTo:RunAs","","RunAs",1)(Window.Close) & GoTo:EOF
+Echo Launcher v1.0.9
+PushD %~DP0 && CD /D "%~DP0"
+: SetLocal EnableDelayedExpansion
+: %1 %2
+: MsHta VbScript:CreateObject("Shell.Application").ShellExecute("%~S0","GoTo:RunAs","","RunAs",1)(Window.Close) && GoTo:EOF
 :RunAs
 CLS
 .\bin\main.exe
