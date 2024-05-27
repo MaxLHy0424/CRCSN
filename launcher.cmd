8e928352af0e612d8169a0af14f889730de78dcf
MaxLHy0424 <MaxLHy974413@outlook.com>
2024-05-27 19:06:46 +0800

    更新启动脚本

--- launcher.cmd
+++ launcher.cmd
@@ -1,6 +1,6 @@
 @Echo Off
-Title CRCSN Launcher v1.0.10
-PushD %~DP0 & CD /D %~DP0
+Title CRCSN Launcher v1.0.11
+CD /D "%~DP0"
 : SetLocal EnableDelayedExpansion
 : %1 %2
 : MsHta VbScript:CreateObject("Shell.Application").ShellExecute("%~S0","GoTo:RunAs","","RunAs",1)(Window.Close) & GoTo:EOF
