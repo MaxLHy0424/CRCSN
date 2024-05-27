183321cc08afaa3bdb033fcafab907689fcda7c5
MaxLHy0424 <MaxLHy974413@outlook.com>
2024-05-27 18:41:18 +0800

    更改几处细节

--- launcher.cmd
+++ launcher.cmd
@@ -1,8 +1,8 @@
 @Echo Off
 Title CRCSN Launcher v1.0.10
-PushD %~DP0 && Cd /D %~DP0
+PushD %~DP0 & CD /D %~DP0
 : SetLocal EnableDelayedExpansion
 : %1 %2
-: MsHta VbScript:CreateObject("Shell.Application").ShellExecute("%~S0","GoTo:RunAs","","RunAs",1)(Window.Close) && GoTo:EOF
+: MsHta VbScript:CreateObject("Shell.Application").ShellExecute("%~S0","GoTo:RunAs","","RunAs",1)(Window.Close) & GoTo:EOF
 :RunAs
 .\bin\main.exe
