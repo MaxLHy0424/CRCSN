91e9c3d1f59c22783a8fbe2d7e50f46fd90d9c66
MaxLHy0424 <MaxLHy974413@outlook.com>
2024-05-30 20:36:04 +0800

    修改默认执行的命令

--- launcher.cmd
+++ launcher.cmd
@@ -1,8 +1,8 @@
 @echo off
-title CRCSN Launcher v1.0.13
+title CRCSN Launcher v1.0.14
 cd /d "%~dp0"
-: setLocal enableDelayedExpansion
-: %1 %2
+setLocal enableDelayedExpansion
+%1 %2
 : msHta vbscript:createObject("shell.application").shellExecute("%~s0","goTo:runAs","","runAs",1)(window.close) & goTo:eof
 :runAs
 .\bin\main.exe
