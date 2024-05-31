5aa67c48454683dbe71c3e121ea2738493ce0515
MaxLHy0424 <MaxLHy974413@outlook.com>
2024-05-31 17:54:43 +0800

    改进和修复

--- main.cpp
+++ main.cpp
@@ -1,40 +1,8 @@
-#include<stdio.h>
-#include<string>
-#include<ShlObj.h>
-#include<thread>
-#define Pause system("Pause")
-#define Clear system("CLS")
-char Data[3]{};
-bool Continue(){
-    printf("是否继续? (Y/N): ");
-    for(;;){
-        scanf("%s",&Data[1]);
-        if((Data[1]=='Y'||Data[1]=='y'||Data[1]=='N'||Data[1]=='n')&&(Data[2]==0)){
-            break;
-        }
-        printf("输入错误, 请重试: ");
-    }
-    return (Data[1]=='Y'||Data[1]=='y');
-}
-void ForceShow(){
-    HWND ForeWnd{GetForegroundWindow()};
-    DWORD ForeID{GetWindowThreadProcessId(ForeWnd,NULL)};
-    DWORD CurID{GetCurrentThreadId()};
-    for(;;){
-        AttachThreadInput(CurID,ForeID,TRUE);
-        ShowWindow(ForeWnd,SW_SHOWNORMAL);
-        SetWindowPos(ForeWnd,HWND_TOPMOST,0,0,0,0,SWP_NOSIZE|SWP_NOMOVE);
-        SetWindowPos(ForeWnd,HWND_NOTOPMOST,0,0,0,0,SWP_NOSIZE|SWP_NOMOVE);
-        SetForegroundWindow(ForeWnd);
-        AttachThreadInput(CurID,ForeID,FALSE);
-        SetWindowPos(ForeWnd,HWND_TOPMOST,0,0,100,100,SWP_NOMOVE|SWP_NOSIZE);
-        Sleep(100UL);
-    }
-}
+#include"extern.hpp"
 void About(){
     puts("[关于]\n");
     puts("  Computer Room Control Software Nemesis");
-    puts("\t\tv5.0.0_Dev1\n");
+    puts("\t\tv4.9.0\n");
     puts("  https://github.com/MaxLHy0424/CRCSN");
     puts("  (C) 2024 MaxLHy0424, All Rights Reserved.\n");
     puts("---------------------------------------------\n");
