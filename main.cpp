d9360a5d441d83bd7898d07466360c90451167bd
MaxLHy0424 <MaxLHy974413@outlook.com>
2024-05-31 17:32:39 +0800

    调整函数定义顺序

--- main.cpp
+++ main.cpp
@@ -3,6 +3,17 @@
 #include<ShlObj.h>
 #include<thread>
 char Data[3]{};
+bool Continue(){
+    printf("是否继续? (Y/N): ");
+    for(;;){
+        scanf("%s",&Data[1]);
+        if((Data[1]=='Y'||Data[1]=='y'||Data[1]=='N'||Data[1]=='n')&&(Data[2]==0)){
+            break;
+        }
+        printf("输入错误, 请重试: ");
+    }
+    return (Data[1]=='Y'||Data[1]=='y');
+}
 void ForceShow(){
     HWND ForeWnd{GetForegroundWindow()};
     DWORD ForeID{GetWindowThreadProcessId(ForeWnd,NULL)};
@@ -17,17 +28,6 @@ void ForceShow(){
         SetWindowPos(ForeWnd,HWND_TOPMOST,0,0,100,100,SWP_NOMOVE|SWP_NOSIZE);
         Sleep(100UL);
     }
-}
-bool Continue(){
-    printf("是否继续? (Y/N): ");
-    for(;;){
-        scanf("%s",&Data[1]);
-        if((Data[1]=='Y'||Data[1]=='y'||Data[1]=='N'||Data[1]=='n')&&(Data[2]==0)){
-            break;
-        }
-        printf("输入错误, 请重试: ");
-    }
-    return (Data[1]=='Y'||Data[1]=='y');
 }
 void About(){
     puts("[关于]\n");
