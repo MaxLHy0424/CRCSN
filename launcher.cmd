efb01febdb77c480f0541971b5eb808a60502265
MaxLHy0424 <MaxLHy974413@outlook.com>
2024-06-01 16:39:16 +0800

    将启动软件的命令放入循环中

--- launcher.cmd
+++ launcher.cmd
@@ -1,7 +1,8 @@
 @Echo Off
-Title CRCSN Launcher v1.0.15
+Title CRCSN Launcher v1.1.0
 CD /D "%~DP0"
 %1 %2
-: MSHTA VBScript:CreateObject("Shell.application").ShellExecute("%~S0","GoTo:RunAs","","RunAs",1)(Window.Close) & GoTo:EOF
+: MSHTA VBScript:CreateObject("Shell.Application").ShellExecute("%~S0","GoTo:RunAs","","RunAs",1)(Window.Close) & GoTo:EOF
 :RunAs
 .\bin\main.exe
+GoTo RunAs
