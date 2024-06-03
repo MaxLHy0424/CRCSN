6ffa2b3670c970359be7523669683b02a549baff
MaxLHy0424 <MaxLHy974413@outlook.com>
2024-06-03 19:50:41 +0800

    更新启动脚本

--- launcher.cmd
+++ launcher.cmd
@@ -1,10 +1,10 @@
 @Echo Off
-Title CRCSN Launcher v1.1.2
-Echo (C) 2023- MaxLHy0424, All Rights Reserved.
+Title CRCSN Launcher v1.1.3
+Echo (C) 2024 MaxLHy0424, All Rights Reserved.
 CD /D "%~DP0"
 %1 %2
 TimeOut /NoBreak /T 3
 : MsHta VBScript:CreateObject("Shell.Application").ShellExecute("%~S0","GoTo:RunAs","","RunAs",1)(Window.Close) & GoTo:EOF
 :RunAs
-.\bin\main.exe
+.\bin\main.exe -fs
 GoTo RunAs
