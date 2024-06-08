421861de2c0746a44648c8d6dda1f3ff9a1ee33d
MaxLHy0424 <MaxLHy974413@outlook.com>
2024-06-08 15:25:42 +0800

    feat(launcher.cmd): 更新启动脚本中的版本号

--- launcher.cmd
+++ launcher.cmd
@@ -1,11 +1,11 @@
 @Echo Off
-PushD "%~DP0"
-%1
+CD /D "%~DP0"
 Title CRCSN Launcher
-Echo         CRCSN Launcher v1.3.0
+Echo         CRCSN Launcher v1.3.1
 Echo.
 Echo (C) 2024 MaxLHy0424, All Rights Reserved.
 TimeOut /NoBreak /T 3
+%1
 : MsHta VBScript:CreateObject("Shell.Application").ShellExecute("%~S0","GoTo:RunAs","","RunAs",1)(Window.Close) & GoTo:EOF
 :RunAs
     .\bin\main.exe
