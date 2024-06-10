e6fed00e66b4fe1d32d0b678ce003898cf1589f7
MaxLHy0424 <MaxLHy974413@outlook.com>
2024-06-11 07:18:26 +0800

    修复 Bug

--- launcher.cmd
+++ launcher.cmd
@@ -1,12 +1,12 @@
 @Echo Off
-CD /D "%~DP0"
 Title CRCSN Launcher
-Echo         CRCSN Launcher v1.3.1
+CD /D "%~DP0"
+%1
+Echo         CRCSN Launcher v1.3.2
 Echo.
 Echo (C) 2024 MaxLHy0424, All Rights Reserved.
 TimeOut /NoBreak /T 3
-%1
-: MsHta VBScript:CreateObject("Shell.Application").ShellExecute("%~S0","GoTo:RunAs","","RunAs",1)(Window.Close) & GoTo:EOF
+: MsHta VbScript:CreateObject("Shell.Application").ShellExecute("%~S0","GoTo:RunAs","","RunAs",1)(Window.Close) & GoTo:EOF
 :RunAs
     .\bin\main.exe
 GoTo RunAs
