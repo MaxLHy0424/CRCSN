06a074c115a91d02d605f1ceb150997e29612599
MaxLHy0424 <MaxLHy974413@outlook.com>
2024-06-15 15:24:27 +0800

    feat(launcher.cmd): 修改输出格式

--- launcher.cmd
+++ launcher.cmd
@@ -3,12 +3,11 @@ Title CRCSN Launcher
 CD /D "%~DP0"
 %1
 Echo.
-Echo           CRCSN Launcher v1.3.4
+Echo           CRCSN Launcher v1.3.5
 Echo.
-Echo (C) 2024 MaxLHy0424, All Rights Reserved.
-Echo. && Echo.
+Echo  (C) 2024 MaxLHy0424, All Rights Reserved.
 TimeOut /NoBreak /T 3
 : MsHta VbScript:CreateObject("Shell.Application").ShellExecute("%~S0","GoTo:RunAs","","RunAs",1)(Window.Close) & GoTo:EOF
 :RunAs
-    .\bin\main.exe
+  .\bin\main.exe
 GoTo RunAs
