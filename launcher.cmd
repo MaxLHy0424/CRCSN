bf5aba85ac076abe8e14cdd009838ef0d0a68a6a
MaxLHy0424 <MaxLHy974413@outlook.com>
2024-06-17 20:01:10 +0800

    feat(launcher): 修复清屏问题

--- launcher.cmd
+++ launcher.cmd
@@ -1,12 +1,13 @@
 @Echo Off
-Title CRCSN Launcher
 CD /D "%~DP0"
 %1
-Echo          CRCSN Launcher v1.3.7
+Title CRCSN Launcher
+Echo          CRCSN Launcher v1.3.8
 Echo.
 Echo (C) 2024 MaxLHy0424, All Rights Reserved.
 TimeOut /NoBreak /T 2
 : MsHta VBScript:CreateObject("Shell.Application").ShellExecute("%~S0","GoTo:RunAs","","RunAs",1)(Window.Close) && GoTo:EOF
 :RunAs
+  CLS
   .\bin\main.exe
 GoTo RunAs
