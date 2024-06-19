8b4048880c180344a611e9a14c352e6a8554a579
MaxLHy0424 <MaxLHy974413@outlook.com>
2024-06-19 17:49:15 +0800

    feat(launcher.cmd): 优化命令执行

--- launcher.cmd
+++ launcher.cmd
@@ -1,12 +1,12 @@
 @Echo Off
+Title CRCSN Launcher
 CD /D "%~DP0"
 %1
-Title CRCSN Launcher
-Echo          CRCSN Launcher v1.3.8
+Echo          CRCSN Launcher v1.3.9
 Echo.
 Echo (C) 2024 MaxLHy0424, All Rights Reserved.
 TimeOut /NoBreak /T 2
-: MsHta VBScript:CreateObject("Shell.Application").ShellExecute("%~S0","GoTo:RunAs","","RunAs",1)(Window.Close) && GoTo:EOF
+: MSHTA VBScript:CreateObject("Shell.Application").ShellExecute("%~S0","GoTo:RunAs","","RunAs",1)(Window.Close) & GoTo:EOF
 :RunAs
   CLS
   .\bin\main.exe
