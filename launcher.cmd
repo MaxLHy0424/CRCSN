b46ef55b0790b532553fdc300a21da813e67b054
MaxLHy0424 <MaxLHy974413@outlook.com>
2024-06-28 17:29:13 +0800

    feat: 修改延迟启动时间

--- launcher.cmd
+++ launcher.cmd
@@ -5,7 +5,7 @@ CD /D "%~DP0"
 Echo          CRCSN Launcher v1.4.0
 Echo.
 Echo (C) 2024 MaxLHy0424, All Rights Reserved.
-TimeOut /NoBreak /T 3
+TimeOut /NoBreak /T 2
 : MSHTA VBScript:CreateObject("Shell.Application").ShellExecute("%~S0","GoTo:RunAs","","RunAs",1)(Window.Close) & GoTo:EOF
 :RunAs
   CLS
