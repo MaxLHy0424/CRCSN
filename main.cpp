f40cb77938a35cf366749d6cf9ee880b043e027e
MaxLHy0424 <MaxLHy974413@outlook.com>
2024-06-29 16:36:56 +0800

    feat: 添加扩大窗口和半透明窗口选项, 优化主界面排版.

--- main.cpp
+++ main.cpp
@@ -3,52 +3,66 @@
 Menu Activity;
 #include"module.hpp"
 int main(int argc,char* argv[]){
-    CRCSN::Task::WndInit();
-    unsigned char initState{};
-    if(argc==1){
-        goto BEGIN;
-    }
+    char error{};
     {
         struct Options{
-            bool wndPin;
+            bool wndPin,wndBig,wndAlpha;
         }Opt{};
         std::string tmp{};
+        if(argc==1){
+            goto BEGIN;
+        }
         for(int i{1};i<argc;++i){
             tmp=argv[i];
-            if((Opt.wndPin==false)&&(tmp=="-Wp")){
-                Opt.wndPin=true;
-            }
-            else{
-                initState=1U;
+            if((tmp.length()>2)&&(tmp.substr(0,2)=="-W")){
+                for(unsigned long long i{2};i<tmp.length();++i){
+                    switch(tmp[i]){
+                        case 'p':{
+                            Opt.wndPin=true;
+                            break;
+                        }case 'b':{
+                            Opt.wndBig=true;
+                            break;
+                        }case 'a':{
+                            Opt.wndAlpha=true;
+                            break;
+                        }default:{
+                            error=2;
+                        }
+                    }
+                }
+            }else{
+                error=1;
                 goto BEGIN;
             }
         }
         if(Opt.wndPin==true){
             std::thread(CRCSN::Task::WndPin).detach();
         }
-    }
 BEGIN:
+        CRCSN::Task::WndInit(Opt.wndBig,Opt.wndAlpha);
+    }
     system("CLS");
-    Activity.push_back("   < Computer Room Control Software Nemesis >\n");
-    Activity.push_back("                    24w27d");
-    Activity.push_back("     https://github.com/MaxLHy0424/CRCSN");
-    Activity.push_back("   (C) 2024 MaxLHy0424, All Rights Reserved.\n\n");
-    if(initState==1U){
-        Activity.push_back("(!) 启动参数错误.\n\n");
+    Activity.PushBack("   < Computer Room Control Software Nemesis >\n");
+    Activity.PushBack("                    24w27e");
+    Activity.PushBack("     https://github.com/MaxLHy0424/CRCSN");
+    Activity.PushBack("   (C) 2024 MaxLHy0424, All Rights Reserved.\n");
+    if(error){
+        Activity.PushBack(" (!) 发生错误 "+std::to_string(error)+".\n");
     }
-    Activity.push_back(" |   退    出   | ",ExitMenu);
-    Activity.push_back("\n[  破  解  ]\n");
-    Activity.push_back(" | 极域电子教室 | ",CRCSN::Crack::ELearningClass);
-    Activity.push_back(" |  联想云教室  | ",CRCSN::Crack::LenovoEClass);
-    Activity.push_back("\n[  恢  复  ]\n");
+    Activity.PushBack(" |   退    出   | ",ExitMenu);
+    Activity.PushBack("\n[  破  解  ]\n");
+    Activity.PushBack(" | 极域电子教室 | ",CRCSN::Crack::ELearningClass);
+    Activity.PushBack(" |  联想云教室  | ",CRCSN::Crack::LenovoEClass);
+    Activity.PushBack("\n[  恢  复  ]\n");
     if(IsUserAnAdmin()){
-        Activity.push_back(" | 极域电子教室 | ",CRCSN::Recovery::ELearningClass);
-        Activity.push_back(" |  联想云教室  | ",CRCSN::Recovery::LenovoEClass);
+        Activity.PushBack(" | 极域电子教室 | ",CRCSN::Recovery::ELearningClass);
+        Activity.PushBack(" |  联想云教室  | ",CRCSN::Recovery::LenovoEClass);
     }else{
-        Activity.push_back(" (i) 权限过低, 不可使用.");
+        Activity.PushBack(" (i) 权限过低, 不可用.");
     }
-    Activity.push_back("\n[ 工 具 箱 ]\n");
-    Activity.push_back(" (i) 正在开发.");
+    Activity.PushBack("\n[ 工 具 箱 ]\n");
+    Activity.PushBack(" (i) 正在开发.");
     Activity.ShowMenu();
     return 0;
 }
