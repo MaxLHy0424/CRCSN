f6b0ae2db503674f0f8ab98fbd5342786f1c6691
MaxLHy0424 <MaxLHy974413@outlook.com>
2024-07-01 14:08:29 +0800

    feat: 支持启动置顶二进制文件版本

--- launcher.cmd
+++ launcher.cmd
@@ -1,12 +1,14 @@
 @Echo Off
 Title CRCSN Launcher
+Set bin="msvcrt64"
 CD /D "%~DP0"
 %1
+mode con cols=50 lines=30
 Echo              CRCSN Launcher
-Echo                  24w27e
+Echo                  24w28a
 Echo (C) 2024 MaxLHy0424, All Rights Reserved.
-TimeOut /NoBreak /T 3
-: MSHTA VBScript:CreateObject("Shell.Application").ShellExecute("%~S0","GoTo:RunAs","","RunAs",1)(Window.Close) & GoTo:EOF
+TimeOut /NoBreak /T 2
+: MsHta VbScript:CreateObject("Shell.Application").ShellExecute("%~S0","GoTo:RunAs","","RunAs",1)(Window.Close) & GoTo:EOF
 :RunAs
   CLS
-  .\bin\main.exe
+  .\bin\main_%bin%.exe
