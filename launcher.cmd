499565c7760b7f1a9880800e09fda5e37cc2e3bb
MaxLHy0424 <MaxLHy974413@outlook.com>
2024-07-02 10:00:54 +0800

    feat(launcher): 优化启动执行

--- launcher.cmd
+++ launcher.cmd
@@ -1,14 +1,15 @@
-@Echo Off
-Title CRCSN Launcher
-Set bin="msvcrt64"
-CD /D "%~DP0"
+@echo off
+title CRCSN Launcher
+set bin = "msvcrt64"
+set branch = "dev"
+cd /d "%~dp0"
 %1
 mode con cols=50 lines=30
-Echo              CRCSN Launcher
-Echo                  24w28a
-Echo (C) 2024 MaxLHy0424, All Rights Reserved.
-TimeOut /NoBreak /T 2
-: MsHta VbScript:CreateObject("Shell.Application").ShellExecute("%~S0","GoTo:RunAs","","RunAs",1)(Window.Close) & GoTo:EOF
+echo              CRCSN Launcher
+echo                  24w28b
+echo (C) 2024 MaxLHy0424, All Rights Reserved.
+timeOut /noBreak /t 2
+: msHta vbScript:createObject("shell.application").shellExecute("%~s0","goTo:runAs","","runAs",1)(window.close) & goTo:EOF
 :RunAs
-  CLS
-  .\bin\main_%bin%.exe
+  cls
+  .\bin\%branch%_%bin%.exe
