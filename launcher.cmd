fde8f784335e8cb21de6f3dec3c8b6850198d8da
MaxLHy0424 <MaxLHy974413@outlook.com>
2024-07-02 14:33:48 +0800

    feat: 优化缩进

--- launcher.cmd
+++ launcher.cmd
@@ -5,9 +5,9 @@ set branch="dev"
 cd /d "%~dp0"
 %1
 mode con cols=50 lines=30
-echo                  CRCSN Launcher
-echo                      24w28b
-echo     (C) 2024 MaxLHy0424, All Rights Reserved.
+echo                 CRCSN Launcher
+echo                     24w28b
+echo    (C) 2024 MaxLHy0424, All Rights Reserved.
 timeOut /noBreak /t 2
 : msHta vbScript:createObject("shell.application").shellExecute("%~s0","goTo:runAs","","runAs",1)(window.close) & goTo:EOF
 :RunAs
