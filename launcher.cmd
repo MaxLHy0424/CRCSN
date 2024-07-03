b08d3e314b02df03881f17b30115ac147c681993
MaxLHy0424 <MaxLHy974413@outlook.com>
2024-07-03 18:29:30 +0800

    feat: 移除倒计时启动, 改为按任意键启动; 修改部分命令执行顺序; 添加字符集切换支持

--- launcher.cmd
+++ launcher.cmd
@@ -1,15 +1,18 @@
 @echo off
-title CRCSN Launcher
+chcp 65001
 set bin="msvcrt64"
 set branch="dev"
 cd /d "%~dp0"
 %1
 mode con cols=50 lines=30
+title CRCSN Launcher
 echo                 CRCSN Launcher
-echo                     24w28b
+echo                     24w28c
 echo    (C) 2024 MaxLHy0424, All Rights Reserved.
-timeOut /noBreak /t 2
+echo.
+echo (i) 按任意键启动 CRCSN... & pause > nul
 : msHta vbScript:createObject("shell.application").shellExecute("%~s0","goTo:runAs","","runAs",1)(window.close) & goTo:EOF
-:RunAs
+:runAs
+  chcp 936
   cls
   .\bin\%branch%_%bin%.exe
