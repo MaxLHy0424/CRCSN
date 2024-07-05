2348460956896a926d1a3b4e9aa8676054849baf
MaxLHy0424 <MaxLHy974413@outlook.com>
2024-07-05 17:40:23 +0800

    feat: 修改软件发行版文件名

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 pathMsys2 = C:\Software\MSYS2
 compiler = g++.exe
 argv = -fexec-charset=GBK -std=gnu++23 -Wall -Wextra -static -Ofast -Os -flto -o
-branch = dev
+os = x86_64
 main:
-	$(pathMsys2)\mingw64\bin\$(compiler) main.cpp $(argv) .\bin\$(branch)_msvcrt64.exe
-	$(pathMsys2)\ucrt64\bin\$(compiler) main.cpp $(argv) .\bin\$(branch)_ucrt64.exe
+	$(pathMsys2)\mingw64\bin\$(compiler) main.cpp $(argv) .\bin\$(os)-msvcrt64.exe
+	$(pathMsys2)\ucrt64\bin\$(compiler) main.cpp $(argv) .\bin\$(os)-ucrt64.exe
--- launcher.cmd
+++ launcher.cmd
@@ -1,18 +1,18 @@
 @echo off
 chcp 65001
+set os="x86_64"
 set bin="msvcrt64"
-set branch="dev"
 cd /d "%~dp0"
 %1
 mode con cols=50 lines=30
 title CRCSN Launcher
 echo                 CRCSN Launcher
-echo                     24w28c
+echo                     24w28d
 echo    (C) 2024 MaxLHy0424, All Rights Reserved.
 echo.
-echo (i) 按任意键启动 CRCSN... & pause > nul
-: msHta vbScript:createObject("shell.application").shellExecute("%~s0","goTo:runAs","","runAs",1)(window.close) & goTo:EOF
+echo 按任意键启动 CRCSN... & pause > nul
+: msHta vbScript:createObject("shell.application").shellExecute("%~s0","goTo:runAs","","runAs",1)(window.close) & goTo:eof
 :runAs
   chcp 936
   cls
-  .\bin\%branch%_%bin%.exe
+  .\bin\%os%-%bin%.exe
