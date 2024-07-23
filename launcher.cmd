2fc1224c73ac8d28c985df8a253ab5e118e6e89b
MaxLHy0424 <MaxLHy974413@outlook.com>
2024-07-23 14:41:58 +0800

    feat: 移除多余引号, 调整命令执行

--- launcher.cmd
+++ launcher.cmd
@@ -1,17 +1,18 @@
 @echo off
-chcp 65001
-set branch="dev"
-set arch="x86_64"
-set bin="msvcrt"
+set branch=dev
+set arch=x86_64
+set bin=msvcrt
 cd /d "%~dp0"
+chcp 65001
 %1
 mode con cols=50 lines=30
 title CRCSN Launcher
 echo                 CRCSN Launcher
-echo                     24w28d
+echo                     24w31b
 echo    (C) 2024 MaxLHy0424, All Rights Reserved.
 echo.
-echo 按任意键启动 CRCSN... & pause > nul
+echo 按任意键启动 CRCSN...
+pause > nul
 : msHta vbScript:createObject("shell.application").shellExecute("%~s0","goTo:runAs","","runAs",1)(window.close) & goTo:eof
 :runAs
 .\bin\%branch%-%arch%-%bin%.exe
