ce1199fe6dba350797ea6921c0a6a7661effb827
MaxLHy0424 <MaxLHy974413@outlook.com>
2024-07-28 15:12:41 +0800

    feat: 优化启动提示

--- launcher.cmd
+++ launcher.cmd
@@ -8,11 +8,10 @@ chcp 65001
 mode con cols=50 lines=25
 title CRCSN Launcher
 echo                  CRCSN Launcher
-echo                      24w31d
+echo                      24w31e
 echo     (C) 2023 MaxLHy0424. All Rights Reserved.
 echo.
-echo 按任意键启动 CRCSN.
-echo.
+echo 按任意键启动 CRCSN (%branch%, %arch%, %bin%).
 pause > nul
 : msHta vbScript:createObject("shell.application").shellExecute("%~s0","goTo:runAs","","runAs",1)(window.close) & goTo:eof
 :runAs
