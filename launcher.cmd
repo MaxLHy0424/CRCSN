40be543d0d9f33a267b4f1c70e7edc77afd5640f
MaxLHy0424 <MaxLHy974413@outlook.com>
2024-07-31 23:02:42 +0800

    change: 修改启动文件

--- launcher.cmd
+++ launcher.cmd
@@ -1,17 +1,18 @@
 @echo off
+set branch=dev
 set arch=x86_64
-set bin=msvcrt
+set runtime=msvcrt
 cd /d "%~dp0"
 chcp 65001
 %1
 mode con cols=50 lines=25
 title CRCSN Launcher
 echo                  CRCSN Launcher
-echo                      24w32a
+echo                      24w32b
 echo     (C) 2023 MaxLHy0424. All Rights Reserved.
 echo.
-echo 按任意键启动 CRCSN (%arch%, %bin%).
+echo 按任意键启动 CRCSN (%branch%, %arch%, %runtime%).
 pause > nul
-: msHta vbScript:createObject("shell.application").shellExecute("%~s0","goTo:runAs","","runAs",1)(window.close) & goTo:eof
+msHta vbScript:createObject("shell.application").shellExecute("%~s0","goTo:runAs","","runAs",1)(window.close) & goTo:eof
 :runAs
-.\bin\CRCSN-%arch%-%bin%.exe
+.\bin\%branch%-%arch%-%runtime%.exe
