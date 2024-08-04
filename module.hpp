53b7571adf09ad5df2a7b5661eacb899b981903d
MaxLHy0424 <MaxLHy974413@outlook.com>
2024-08-04 09:31:04 +0800

    change: 修改函数

--- module.hpp
+++ module.hpp
@@ -39,7 +39,7 @@ namespace CRCSN{
             SetConsoleTitle("CRCSN");
         #endif
         if(!ctrls){
-            SetWindowLongPtrA(GetConsoleWindow(),GWL_STYLE,GetWindowLongPtrA(GetConsoleWindow(),GWL_STYLE)&~WS_SIZEBOX&~WS_MAXIMIZEBOX&~WS_MINIMIZEBOX);
+            SetWindowLongPtr(GetConsoleWindow(),GWL_STYLE,GetWindowLongPtrA(GetConsoleWindow(),GWL_STYLE)&~WS_SIZEBOX&~WS_MAXIMIZEBOX&~WS_MINIMIZEBOX);
         }
         system("mode con cols=50 lines=25");
         SetLayeredWindowAttributes(GetForegroundWindow(),0UL,((alpha)?(230U):(255U)),LWA_ALPHA);
