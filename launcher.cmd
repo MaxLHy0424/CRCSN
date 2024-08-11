da81a5f84470f87fe8910e41be7e23bbe05bc969
MaxLHy0424 <MaxLHy974413@outlook.com>
2024-08-11 09:28:18 +0800

    change: 抛弃分支区分, 改为使用 start 命令启动, 屏蔽字符集更改输出

--- launcher.cmd
+++ launcher.cmd
@@ -1,25 +1,24 @@
 @echo off
-chcp 65001
-set EVALUATE_COPY=true
-if "%EVALUATE_COPY%"=="true" (
-    title [评估副本] CRCSN Launcher
-) ^
-else (
+chcp 65001 > nul
+set EVALUATE_COPY=1
+if %EVALUATE_COPY%==0 (
     title CRCSN Launcher
+) else (
+    title [评估副本] CRCSN Launcher
 )
 mode con cols=50 lines=25
-set branch=dev
 set arch=x86_64
 set runtime=msvcrt
 set argv=
 cd /d "%~dp0"
 %1
 echo                  CRCSN Launcher
-echo                   v2.0.0-beta.1
+echo                   v2.0.0-beta.2
 echo     (C) 2023 MaxLHy0424. All Rights Reserved.
 echo.
-echo 按任意键启动 CRCSN (%branch%, %arch%, %runtime%)...
+echo 按任意键启动 CRCSN (%arch%, %runtime%)...
 pause > nul
 msHta vbScript:createObject("shell.application").shellExecute("%~s0","goTo:runAs","","runAs",1)(window.close) & goTo:eof
 :runAs
-    .\bin\%branch%-%arch%-%runtime%.exe %argv%
+    start .\bin\CRCSN-%arch%-%runtime%.exe %argv% /command
+    exit /b 0
