29e42c584100abc86e890a2b1eeb5808dc53245a
MaxLHy0424 <MaxLHy974413@outlook.com>
2024-08-20 14:57:47 +0800

    style

--- src/header.hpp
+++ src/header.hpp
@@ -1,12 +1,12 @@
-#define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
 #include<string>
 #include<thread>
 #include<vector>
 #include<shlobj.h>
+#define _CRT_SECURE_NO_WARNINGS
+#define CHANNEL 2
 typedef const char* cstr;
 typedef int i32;
 typedef short i16;
 typedef unsigned long long u64;
 typedef unsigned short u16;
-#define CHANNEL 2
