282864dc79a8826cbc50dd4b160678383e669a6d
MaxLHy0424 <MaxLHy974413@outlook.com>
2024-08-27 08:24:06 +0800

    change: 版本号

--- launcher.cmd
+++ launcher.cmd
@@ -18,13 +18,13 @@ set argv=
 cd /d "%~dp0"
 %1
 echo                  CRCSN Launcher
-echo                      v2.0.1
+echo                      v2.0.2
 echo     (C) 2023 MaxLHy0424. All Rights Reserved.
 echo.
-echo (i) 按任意键启动 CRCSN (%arch%, %runtime%).
+echo 按任意键启动 CRCSN (%arch%, %runtime%)...
 pause > nul
 msHta vbScript:createObject("shell.application").shellExecute("%~s0","goTo:runAs","","runAs",1)(window.close) & goTo:eof
 :runAs
-  echo (i) 请稍候...
+  echo (i) 正在启动, 请稍候.
   start .\bin\%arch%\main-%runtime%.exe %argv% /command
   exit
