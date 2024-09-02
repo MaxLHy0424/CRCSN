0eef8ff59a87f15d22e93fe0070229422a997483
MaxLHy0424 <MaxLHy974413@outlook.com>
2024-09-02 19:03:57 +0800

    refactor: 优化信息显示

--- launcher.cmd
+++ launcher.cmd
@@ -21,8 +21,9 @@ cd /d "%~dp0"
 echo                  CRCSN Launcher
 echo                      v5.2.0
 echo     (C) 2023 MaxLHy0424, All Rights Reserved.
+echo.
+echo (i) CRCSN: v5.2.0, %arch%, %runtime%.
 if %AUTO_LAUNCH%==0 (
-  echo.
   echo 按任意键启动 CRCSN...
   pause > nul
 )
