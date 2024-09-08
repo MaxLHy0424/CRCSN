d95dcd47bf94bdc166f7ef0bd7409516cc34f212
MaxLHy0424 <MaxLHy974413@outlook.com>
2024-09-08 16:47:28 +0800

    rm: 版本号

--- launcher.cmd
+++ launcher.cmd
@@ -23,7 +23,7 @@ if %AUTO_LAUNCH%==0 (
   echo.
   echo     ^(C^) 2023 MaxLHy0424. All rights reserved.
   echo.
-  echo 按任意键启动 CRCSN ^(v5.3.3, %arch%, %runtime%^)...
+  echo 按任意键启动 CRCSN ^(%arch%, %runtime%^)...
   pause > nul
 )
 msHta vbScript:createObject("shell.application").shellExecute("%~s0","goTo:runAs","","runAs",1)(window.close) & goTo:eof
