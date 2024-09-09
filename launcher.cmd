5ce1ddfd43cbc42451ddcb98548ddf4eda5a6c8d
MaxLHy0424 <MaxLHy974413@outlook.com>
2024-09-09 21:04:23 +0800

    refactor: 变量定义顺序

--- launcher.cmd
+++ launcher.cmd
@@ -1,8 +1,11 @@
 @echo off
 chcp 65001 > nul
+set arch=x86_64
+set runtime=msvcrt
+set argv=
 set AUTO_LAUNCH=0
 set CHANNEL=1
-set CUSTOM_TITLE=[<CUSTOM>] CRCSN Launcher
+set CUSTOM_TITLE=[[CUSTOM]] CRCSN Launcher
 if %CHANNEL%==1 (
   title CRCSN Launcher
 ) else if %CHANNEL%==2 (
@@ -13,9 +16,6 @@ if %CHANNEL%==1 (
   title %CUSTOM_TITLE%
 )
 mode con cols=50 lines=25
-set arch=x86_64
-set runtime=msvcrt
-set argv=
 cd /d "%~dp0"
 %1
 if %AUTO_LAUNCH%==0 (
@@ -29,4 +29,3 @@ if %AUTO_LAUNCH%==0 (
 msHta vbScript:createObject("shell.application").shellExecute("%~s0","goTo:runAs","","runAs",1)(window.close) & goTo:eof
 :runAs
   start .\bin\%arch%\CRCSN-%runtime%.exe %argv%
-  exit /b %CHANNEL%
