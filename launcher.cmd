28eb44d1ed5d4a8b96532a2068db82b8038080aa
MaxLHy0424 <MaxLHy974413@outlook.com>
2024-09-15 17:06:56 +0800

    change: text

--- launcher.cmd
+++ launcher.cmd
@@ -18,7 +18,7 @@ set runtime=msvcrt
 set argv=
 %1
 if %AUTO_LAUNCH%==0 (
-  echo                ^< CRCSN Launcher ^>
+  echo                 [ CRCSN Launcher ]
   echo.
   echo     ^(C^) 2023 MaxLHy0424. All rights reserved.
   echo.
