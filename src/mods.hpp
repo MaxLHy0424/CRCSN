4200e9cb8fb3469692f6b7871596b44ba70a7768
MaxLHy0424 <MaxLHy974413@outlook.com>
2024-09-15 16:24:25 +0800

    revert

--- src/mods.hpp
+++ src/mods.hpp
@@ -18,8 +18,8 @@ namespace Mod{
             (GetWindowLongPtr(GetConsoleWindow(),GWL_STYLE)|WS_SIZEBOX|WS_MAXIMIZEBOX):
             (GetWindowLongPtr(GetConsoleWindow(),GWL_STYLE)&~WS_SIZEBOX&~WS_MAXIMIZEBOX)
         );
-        SetLayeredWindowAttributes(GetConsoleWindow(),0,(opt.wndAlpha)?(204):(255),LWA_ALPHA);
         system("mode con cols=50 lines=25");
+        SetLayeredWindowAttributes(GetConsoleWindow(),0,(opt.wndAlpha)?(204):(255),LWA_ALPHA);
     }
     auto frontShow(){
         HWND foreWnd{GetConsoleWindow()};
