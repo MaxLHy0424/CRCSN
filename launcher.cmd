b60e41ea70b6aaa78e866f608a911e129328c88f
MaxLHy0424 <MaxLHy974413@outlook.com>
2024-09-15 16:49:18 +0800

    feat: 显示详细信息

--- launcher.cmd
+++ launcher.cmd
@@ -1,6 +1,7 @@
 @echo off
 chcp 65001 > nul
 set AUTO_LAUNCH=0
+set SHOW_VERBOSE_INFO=0
 set CHANNEL=0
 set CUSTOM_TITLE=[#CUSTOM#] CRCSN Launcher
 if %CHANNEL%==0 (
@@ -21,7 +22,11 @@ if %AUTO_LAUNCH%==0 (
   echo.
   echo     ^(C^) 2023 MaxLHy0424. All rights reserved.
   echo.
-  echo 按任意键启动 CRCSN ^(%arch%, %runtime%^)...
+  if %SHOW_VERBOSE_INFO%==0 (
+    echo 按任意键启动 CRCSN...
+  ) else (
+    echo 按任意键启动 CRCSN ^(%arch%, %runtime%^)...
+  )
   pause > nul
 )
 msHta vbScript:createObject("shell.application").shellExecute("%~s0","goTo:runAs","","runAs",1)(window.close) & goTo:eof
