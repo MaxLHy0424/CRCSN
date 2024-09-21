1448f780a691f30e87afa28818291b3be1988190
MaxLHy0424 <MaxLHy974413@outlook.com>
2024-09-22 07:34:44 +0800

    change: text

--- src/mod.hpp
+++ src/mod.hpp
@@ -35,7 +35,7 @@ namespace Mod{
     auto info(Data){
         CUI ui;
         ui.add("                    | 关  于 |\n\n")
-          .add(" > 返回 ",Mod::exit,nullptr,CON_RED_PALE)
+          .add(" < 返回 ",Mod::exit,nullptr,CON_RED_PALE)
           .add("\n[  名  称  ]\n")
           .add(" " INFO_NAME)
           .add(" " INFO_FULL_NAME)
