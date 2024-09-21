6ab03a2e3ab89640e5cec25cfe8f8546f6f00353
MaxLHy0424 <MaxLHy974413@outlook.com>
2024-09-21 18:22:54 +0800

    refactor

--- src/def.hpp
+++ src/def.hpp
@@ -13,10 +13,10 @@ using i16=short;
 using i32=int;
 #define WINDOW_TITLE "CRCSN"
 #endif
-#define INFO_NAME "CRCSN"
-#define INFO_FULL_NAME "Computer Room Control Software Nemesis"
-#define INFO_VERSION "v5.6.2 (Stable)"
-#define INFO_REPO "https://github.com/MaxLHy0424/CRCSN"
-#define INFO_PUBLISHER "MaxLHy0424"
-#define INFO_LICENSE "MIT License"
-#define INFO_COPYRIGHT "(C) 2023 MaxLHy0424. All rights reserved."
+#define SOFTWARE_INFO_NAME "CRCSN"
+#define SOFTWARE_INFO_FULL_NAME "Computer Room Control Software Nemesis"
+#define SOFTWARE_INFO_VERSION "v5.6.2 (Stable)"
+#define SOFTWARE_INFO_REPO "https://github.com/MaxLHy0424/CRCSN"
+#define SOFTWARE_INFO_PUBLISHER "MaxLHy0424"
+#define SOFTWARE_INFO_LICENSE "MIT License"
+#define SOFTWARE_INFO_COPYRIGHT "(C) 2023 MaxLHy0424. All rights reserved."
--- src/mod.hpp
+++ src/mod.hpp
@@ -37,14 +37,14 @@ namespace Mod{
         ui.add("                    | 关  于 |\n\n")
           .add(" > 返回 ",Mod::exit,nullptr,CON_RED_PALE)
           .add("\n[名称]\n")
-          .add(" " INFO_FULL_NAME)
+          .add(" " SOFTWARE_INFO_FULL_NAME)
           .add("\n[版本]\n")
-          .add(" " INFO_VERSION)
+          .add(" " SOFTWARE_INFO_VERSION)
           .add("\n[仓库]\n")
-          .add(" " INFO_REPO)
+          .add(" " SOFTWARE_INFO_REPO)
           .add("\n[许可证]\n")
-          .add(" " INFO_LICENSE)
-          .add(" " INFO_COPYRIGHT)
+          .add(" " SOFTWARE_INFO_LICENSE)
+          .add(" " SOFTWARE_INFO_COPYRIGHT)
           .show();
         return false;
     }
