6b483877060a7ca63aeeb19bfc36170c32772e03
MaxLHy0424 <MaxLHy974413@outlook.com>
2024-09-22 07:45:58 +0800

    change: text

--- launcher.cmd
+++ launcher.cmd
@@ -12,7 +12,7 @@ set runtime=msvcrt
 set args=
 %1
 if %AUTO_LAUNCH%==0 (
-  echo                    ^| Launcher ^|
+  echo                      Launcher
   echo.
   echo     ^(C^) 2023 MaxLHy0424. All rights reserved.
   echo.
