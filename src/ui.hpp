4f3f7fe8a20f8084557f9f370cc0744d8202f5d3
MaxLHy0424 <MaxLHy974413@outlook.com>
2024-09-22 23:03:07 +0800

    style

--- src/ui.hpp
+++ src/ui.hpp
@@ -22,207 +22,207 @@ struct Data final{
         eventFlag{mouseEvent.dwEventFlags},ui{ui},args{args}{}
 };
 class UI final{
-    private:
-        using call=bool(*)(Data);
-        struct Item final{
-            const i8 *text;
-            i16 colorDef,colorHighlight,colorLast;
-            COORD pos;
-            call fn;
-            void *args;
-            Item():
-                text{},colorDef{CON_WHITE},colorHighlight{CON_BLUE},
-                colorLast{CON_WHITE},pos{},fn{},args{}{}
-            Item(const i8 *text,i16 def,i16 highlight,call fn,void *args):
-                text{text},colorDef{def},colorHighlight{highlight},
-                colorLast{CON_WHITE},pos{},fn{fn},args{args}{}
-            auto setColor(i8 k){
-                switch(k){
-                    case 'd':{
-                        SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),colorDef);
-                        colorLast=colorDef;
-                        break;
-                    }case 'h':{
-                        SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),colorHighlight);
-                        colorLast=colorHighlight;
-                        break;
-                    }
-                }
-            }
-            auto operator==(const COORD &mousePos)const{
-                return (pos.Y==mousePos.Y)&&(pos.X<=mousePos.X)&&(mousePos.X<(pos.X+(i16)strlen(text)));
-            }
-            auto operator!=(const COORD &mousePos)const{
-                return !operator==(mousePos);
-            }
-        };
-        i16 height,width;
-        std::vector<Item> item;
-        auto opCursor(char k){
-            CONSOLE_CURSOR_INFO cursorInfo;
-            GetConsoleCursorInfo(GetStdHandle(STD_OUTPUT_HANDLE),&cursorInfo);
+private:
+    using call=bool(*)(Data);
+    struct Item final{
+        const i8 *text;
+        i16 colorDef,colorHighlight,colorLast;
+        COORD pos;
+        call fn;
+        void *args;
+        Item():
+            text{},colorDef{CON_WHITE},colorHighlight{CON_BLUE},
+            colorLast{CON_WHITE},pos{},fn{},args{}{}
+        Item(const i8 *text,i16 def,i16 highlight,call fn,void *args):
+            text{text},colorDef{def},colorHighlight{highlight},
+            colorLast{CON_WHITE},pos{},fn{fn},args{args}{}
+        auto setColor(i8 k){
             switch(k){
-                case 'h':{
-                    cursorInfo.bVisible=false;
+                case 'd':{
+                    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),colorDef);
+                    colorLast=colorDef;
                     break;
-                }case 's':{
-                    cursorInfo.bVisible=true;
-                    break;
-                }
-            }
-            SetConsoleCursorInfo(GetStdHandle(STD_OUTPUT_HANDLE),&cursorInfo);
-        }
-        auto opAttrs(char k){
-            DWORD mode;
-            GetConsoleMode(GetStdHandle(STD_INPUT_HANDLE),&mode);
-            switch(k){
-                case '+':{
-                    mode|=ENABLE_QUICK_EDIT_MODE,mode|=ENABLE_INSERT_MODE,mode|=ENABLE_MOUSE_INPUT;
-                    break;
-                }case '-':{
-                    mode&=~ENABLE_QUICK_EDIT_MODE,mode&=~ENABLE_INSERT_MODE,mode|=ENABLE_MOUSE_INPUT;
+                }case 'h':{
+                    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),colorHighlight);
+                    colorLast=colorHighlight;
                     break;
                 }
             }
-            SetConsoleMode(GetStdHandle(STD_INPUT_HANDLE),mode);
         }
-        auto getCursor(){
-            CONSOLE_SCREEN_BUFFER_INFO tmp;
-            GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE),&tmp);
-            return tmp.dwCursorPosition;
+        auto operator==(const COORD &mousePos)const{
+            return (pos.Y==mousePos.Y)&&(pos.X<=mousePos.X)&&(mousePos.X<(pos.X+(i16)strlen(text)));
         }
-        auto setCursor(const COORD &tmp={0,0}){
-            SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE),tmp);
+        auto operator!=(const COORD &mousePos)const{
+            return !operator==(mousePos);
         }
-        auto waitMouseEvent(bool move=true){
-            INPUT_RECORD record;
-            DWORD reg;
-            while(true){
-                Sleep(10);
-                ReadConsoleInput(GetStdHandle(STD_INPUT_HANDLE),&record,1,&reg);
-                if((record.EventType==MOUSE_EVENT)&&(move|(record.Event.MouseEvent.dwEventFlags!=MOUSE_MOVED))){
-                    return record.Event.MouseEvent;
-                }
+    };
+    i16 height,width;
+    std::vector<Item> item;
+    auto opCursor(char k){
+        CONSOLE_CURSOR_INFO cursorInfo;
+        GetConsoleCursorInfo(GetStdHandle(STD_OUTPUT_HANDLE),&cursorInfo);
+        switch(k){
+            case 'h':{
+                cursorInfo.bVisible=false;
+                break;
+            }case 's':{
+                cursorInfo.bVisible=true;
+                break;
             }
         }
-        auto getConsoleSize(){
-            CONSOLE_SCREEN_BUFFER_INFO tmp;
-            GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE),&tmp);
-            height=tmp.dwSize.Y;
-            width=tmp.dwSize.X;
-        }
-        auto cls(){
-            getConsoleSize();
-            setCursor({0,0});
-            printf("%s",std::string(width*height,' ').c_str());
-            setCursor({0,0});
-        }
-        auto write(const i8 *text,bool isEndl=false){
-            printf("%s",text);
-            if(isEndl){
-                printf("\n");
+        SetConsoleCursorInfo(GetStdHandle(STD_OUTPUT_HANDLE),&cursorInfo);
+    }
+    auto opAttrs(char k){
+        DWORD mode;
+        GetConsoleMode(GetStdHandle(STD_INPUT_HANDLE),&mode);
+        switch(k){
+            case '+':{
+                mode|=ENABLE_QUICK_EDIT_MODE,mode|=ENABLE_INSERT_MODE,mode|=ENABLE_MOUSE_INPUT;
+                break;
+            }case '-':{
+                mode&=~ENABLE_QUICK_EDIT_MODE,mode&=~ENABLE_INSERT_MODE,mode|=ENABLE_MOUSE_INPUT;
+                break;
             }
         }
-        auto rewrite(Item &ref){
-            setCursor({0,ref.pos.Y});
-            for(i16 j{};j<ref.pos.X;++j){
-                write(" ");
+        SetConsoleMode(GetStdHandle(STD_INPUT_HANDLE),mode);
+    }
+    auto getCursor(){
+        CONSOLE_SCREEN_BUFFER_INFO tmp;
+        GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE),&tmp);
+        return tmp.dwCursorPosition;
+    }
+    auto setCursor(const COORD &tmp={0,0}){
+        SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE),tmp);
+    }
+    auto waitMouseEvent(bool move=true){
+        INPUT_RECORD record;
+        DWORD reg;
+        while(true){
+            Sleep(10);
+            ReadConsoleInput(GetStdHandle(STD_INPUT_HANDLE),&record,1,&reg);
+            if((record.EventType==MOUSE_EVENT)&&(move|(record.Event.MouseEvent.dwEventFlags!=MOUSE_MOVED))){
+                return record.Event.MouseEvent;
             }
-            setCursor({0,ref.pos.Y});
-            write(ref.text);
-            setCursor({0,ref.pos.Y});
         }
-        auto initPos(){
-            cls();
-            for(auto &ref:item){
-                ref.pos=getCursor();
+    }
+    auto getConsoleSize(){
+        CONSOLE_SCREEN_BUFFER_INFO tmp;
+        GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE),&tmp);
+        height=tmp.dwSize.Y;
+        width=tmp.dwSize.X;
+    }
+    auto cls(){
+        getConsoleSize();
+        setCursor({0,0});
+        printf("%s",std::string(width*height,' ').c_str());
+        setCursor({0,0});
+    }
+    auto write(const i8 *text,bool isEndl=false){
+        printf("%s",text);
+        if(isEndl){
+            printf("\n");
+        }
+    }
+    auto rewrite(Item &ref){
+        setCursor({0,ref.pos.Y});
+        for(i16 j{};j<ref.pos.X;++j){
+            write(" ");
+        }
+        setCursor({0,ref.pos.Y});
+        write(ref.text);
+        setCursor({0,ref.pos.Y});
+    }
+    auto initPos(){
+        cls();
+        for(auto &ref:item){
+            ref.pos=getCursor();
+            ref.setColor('d');
+            write(ref.text,true);
+        }
+    }
+    auto refresh(COORD &hangPos){
+        for(auto &ref:item){
+            if((ref==hangPos)&&(ref.colorLast!=ref.colorHighlight)){
+                ref.setColor('h');
+                rewrite(ref);
+            }
+            if((ref!=hangPos)&&(ref.colorLast!=ref.colorDef)){
                 ref.setColor('d');
-                write(ref.text,true);
+                rewrite(ref);
             }
         }
-        auto refresh(COORD &hangPos){
-            for(auto &ref:item){
-                if((ref==hangPos)&&(ref.colorLast!=ref.colorHighlight)){
-                    ref.setColor('h');
-                    rewrite(ref);
-                }
-                if((ref!=hangPos)&&(ref.colorLast!=ref.colorDef)){
+    }
+    auto impl(MOUSE_EVENT_RECORD &mouseEvent){
+        bool isExit{};
+        for(auto &ref:item){
+            if(ref==mouseEvent.dwMousePosition){
+                if(ref.fn!=nullptr){
+                    cls();
                     ref.setColor('d');
-                    rewrite(ref);
+                    opAttrs('+');
+                    opCursor('s');
+                    isExit=ref.fn(Data(mouseEvent,this,ref.args));
+                    opAttrs('-');
+                    opCursor('h');
+                    initPos();
                 }
+                break;
             }
         }
-        auto impl(MOUSE_EVENT_RECORD &mouseEvent){
-            bool isExit{};
-            for(auto &ref:item){
-                if(ref==mouseEvent.dwMousePosition){
-                    if(ref.fn!=nullptr){
-                        cls();
-                        ref.setColor('d');
-                        opAttrs('+');
-                        opCursor('s');
-                        isExit=ref.fn(Data(mouseEvent,this,ref.args));
-                        opAttrs('-');
-                        opCursor('h');
-                        initPos();
+        return isExit;
+    }
+public:
+    UI():
+        height{},width{}{}
+    ~UI(){}
+    auto size(){
+        return item.size();
+    }
+    auto &add(const i8 *text,call fn=nullptr,void *args=nullptr,i16 colorHighlight=CON_BLUE,i16 colorDef=CON_WHITE){
+        item.emplace_back(Item(text,colorDef,(fn==nullptr)?(colorDef):(colorHighlight),fn,args));
+        return *this;
+    }
+    auto &insert(int idx,const i8 *text,call fn=nullptr,void *args=nullptr,i16 colorHighlight=CON_BLUE,i16 colorDef=CON_WHITE){
+        item.emplace(item.begin()+idx,Item(text,colorDef,(fn==nullptr)?(colorDef):(colorHighlight),fn,args));
+        return *this;
+    }
+    auto &edit(int idx,const i8 *text,call fn=nullptr,void *args=nullptr,i16 colorHighlight=CON_BLUE,i16 colorDef=CON_WHITE){
+        item[idx]={text,colorDef,(fn==nullptr)?(colorDef):(colorHighlight),fn,args};
+        return *this;
+    }
+    auto &remove(){
+        item.pop_back();
+        return *this;
+    }
+    auto &remove(int begin,int end){
+        item.erase(item.begin()+begin,item.begin()+end);
+        return *this;
+    }
+    auto &clear(){
+        item.clear();
+        return *this;
+    }
+    auto show(){
+        opAttrs('-');
+        opCursor('h');
+        MOUSE_EVENT_RECORD mouseEvent;
+        initPos();
+        bool isExit{};
+        while(!isExit){
+            mouseEvent=waitMouseEvent();
+            switch(mouseEvent.dwEventFlags){
+                case MOUSE_MOVE:{
+                    refresh(mouseEvent.dwMousePosition);
+                    break;
+                }case MOUSE_CLICK:{
+                    if((mouseEvent.dwButtonState)&&(mouseEvent.dwButtonState!=MOUSE_WHEEL)){
+                        isExit=impl(mouseEvent);
                     }
                     break;
                 }
             }
-            return isExit;
+            Sleep(50);
         }
-    public:
-        UI():
-            height{},width{}{}
-        ~UI(){}
-        auto size(){
-            return item.size();
-        }
-        auto &add(const i8 *text,call fn=nullptr,void *args=nullptr,i16 colorHighlight=CON_BLUE,i16 colorDef=CON_WHITE){
-            item.emplace_back(Item(text,colorDef,(fn==nullptr)?(colorDef):(colorHighlight),fn,args));
-            return *this;
-        }
-        auto &insert(int idx,const i8 *text,call fn=nullptr,void *args=nullptr,i16 colorHighlight=CON_BLUE,i16 colorDef=CON_WHITE){
-            item.emplace(item.begin()+idx,Item(text,colorDef,(fn==nullptr)?(colorDef):(colorHighlight),fn,args));
-            return *this;
-        }
-        auto &edit(int idx,const i8 *text,call fn=nullptr,void *args=nullptr,i16 colorHighlight=CON_BLUE,i16 colorDef=CON_WHITE){
-            item[idx]={text,colorDef,(fn==nullptr)?(colorDef):(colorHighlight),fn,args};
-            return *this;
-        }
-        auto &remove(){
-            item.pop_back();
-            return *this;
-        }
-        auto &remove(int begin,int end){
-            item.erase(item.begin()+begin,item.begin()+end);
-            return *this;
-        }
-        auto &clear(){
-            item.clear();
-            return *this;
-        }
-        auto show(){
-            opAttrs('-');
-            opCursor('h');
-            MOUSE_EVENT_RECORD mouseEvent;
-            initPos();
-            bool isExit{};
-            while(!isExit){
-                mouseEvent=waitMouseEvent();
-                switch(mouseEvent.dwEventFlags){
-                    case MOUSE_MOVE:{
-                        refresh(mouseEvent.dwMousePosition);
-                        break;
-                    }case MOUSE_CLICK:{
-                        if((mouseEvent.dwButtonState)&&(mouseEvent.dwButtonState!=MOUSE_WHEEL)){
-                            isExit=impl(mouseEvent);
-                        }
-                        break;
-                    }
-                }
-                Sleep(50);
-            }
-            cls();
-        }
+        cls();
+    }
 };
