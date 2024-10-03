e85acd3d5fe5a2d7aff23d6a936cb3eed5ac1d66
MaxLHy0424 <MaxLHy974413@outlook.com>
2024-10-03 20:07:36 +0800

    rm: flag

--- src/def.hpp
+++ src/def.hpp
@@ -20,7 +20,7 @@ using i16=short;
 using i32=int;
 #endif
 #define INFO_NAME "Computer Room Control Software Nemesis"
-#define INFO_VERSION "v5.7.2 (Stable)"
+#define INFO_VERSION "v5.7.2"
 #define INFO_REPO_URL "https://github.com/MaxLHy0424/CRCSN"
 #define INFO_DEVELOPER "MaxLHy0424"
 #define INFO_LICENSE "MIT License"
