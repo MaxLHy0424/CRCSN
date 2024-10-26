68cadc008c56de0dc7e825894b965ad428b0a6f2
MaxLHy0424 <MaxLHy974413@outlook.com>
2024-10-26 20:09:33 +0800

    feat: v6.0 版本号

--- src/def.hpp
+++ src/def.hpp
@@ -12,7 +12,11 @@
 #include<thread>
 #endif
 #define INFO_NAME "Computer Room Control Software Nemesis"
+#ifdef _THE_NEXT_MAJOR_UPDATE_
+#define INFO_VERSION "v6.0.0-dev.241026"
+#else
 #define INFO_VERSION "v5.10.0"
+#endif
 #define INFO_REPO_URL "https://github.com/MaxLHy0424/CRCSN"
 #define INFO_DEVELOPER "MaxLHy0424"
 #define INFO_LICENSE "MIT License"
