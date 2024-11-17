3f611469dc6195a71c4cb6dfd31abee5a4ec63ea
MaxLHy0424 <MaxLHy974413@outlook.com>
2024-11-17 21:47:15 +0800

    refactor

--- launcher.cmd
+++ launcher.cmd
@@ -3,11 +3,11 @@ chcp 65001 > nul
 set config.auto_launch=0
 set config.show_verbose_info=0
 set config.window_title=CRCSN Launcher
-set item.tag=std
-set item.version=v5.10.3
-set item.arch=x86_64
-set item.runtime=ucrt
-set item.args=
+set launch.tag=std
+set launch.version=v5.10.3
+set launch.arch=x86_64
+set launch.runtime=ucrt
+set launch.args=
 title %config.window_title%
 mode con cols=50 lines=25
 cd /d "%~dp0"
@@ -20,11 +20,11 @@ if %config.auto_launch%==0 (
   if %config.show_verbose_info%==0 (
     echo 按任意键启动 CRCSN.
   ) else (
-    echo 按任意键启动 CRCSN ^(%item.tag%, %item.version%, %item.arch%, %item.runtime%^).
+    echo 按任意键启动 CRCSN ^(%launch.tag%, %launch.version%, %launch.arch%, %launch.runtime%^).
   )
   pause > nul
 )
 msHta vbScript:createObject("shell.application").shellExecute("%~s0","goTo:launch","","runAs",1)(window.close) & goTo:eof
 :launch
-  start .\bin\%item.version%\%item.tag%-%item.arch%-%item.runtime%.exe %item.args%
+  start .\bin\%launch.version%\%launch.tag%-%launch.arch%-%launch.runtime%.exe %launch.args%
   exit /b
