b3e419dfa9d39d3f3859fd085ea700364f0e69f1
MaxLHy0424 <MaxLHy974413@outlook.com>
2024-11-26 00:13:12 +0800

    v5.10.5 / v6.0.0-preview.5

--- src/info.hpp
+++ src/info.hpp
@@ -1,14 +1,14 @@
 #pragma once
 #define INFO_NAME "Computer Room Control Software Nemesis"
 #ifdef _NEXT_
-#define INFO_VERSION "v6.0.0-preview.4"
+# define INFO_VERSION "v6.0.0-preview.5"
 #else
-#define INFO_VERSION "v5.10.4"
+# define INFO_VERSION "v5.10.5"
 #endif
-#define INFO_REPO_URL "https://github.com/MaxLHy0424/CRCSN"
+#define INFO_REPO_URL  "https://github.com/MaxLHy0424/CRCSN"
 #define INFO_DEVELOPER "MaxLHy0424"
-#define INFO_LICENSE "MIT License"
+#define INFO_LICENSE   "MIT License"
 #ifdef _NEXT_
-#define WINDOW_WIDTH 50
-#define WINDOW_HEIGHT 25
+# define WINDOW_WIDTH  50
+# define WINDOW_HEIGHT 25
 #endif
