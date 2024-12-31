8ac5bd089359847700dd9b2ddb02e625968c1221
MaxLHy0424 <MaxLHy974413@outlook.com>
2024-12-31 19:31:55 +0800

    fix(core.hpp): 优化线程休眠时间表示

--- src/core.hpp
+++ src/core.hpp
@@ -305,7 +305,7 @@ namespace core {
                       HKEY_LOCAL_MACHINE,
                       std::format( R"(SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\{})", exec ).c_str() );
                 }
-                std::this_thread::sleep_for( 1000ms );
+                std::this_thread::sleep_for( 1s );
             }
         }
       public:
