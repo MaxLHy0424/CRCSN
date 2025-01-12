5fec8464ecc026e083d4c1b1e53d67b2a219beed
MaxLHy0424 <MaxLHy974413@outlook.com>
2025-01-12 23:28:53 +0800

    fix(cpp_utils): 字符串视图字串处理问题

--- src/cpp_utils.hpp
+++ src/cpp_utils.hpp
@@ -154,13 +154,15 @@ namespace cpp_utils {
         requires( is_convertible_char_type< _target_char_type_, _source_char_type_ >() )
     inline auto string_convert( const std_string< _source_char_type_ > &_str )
     {
-        return std_string< _target_char_type_ >{ reinterpret_cast< const _target_char_type_ * >( _str.c_str() ) };
+        auto str_it{ reinterpret_cast< const _target_char_type_ * >( _str.cbegin() ) };
+        return std_string< _target_char_type_ >{ str_it, str_it + _str.size() };
     }
     template < typename _target_char_type_, typename _source_char_type_ >
         requires( is_convertible_char_type< _target_char_type_, _source_char_type_ >() )
     inline auto string_convert( const std_string_view< _source_char_type_ > _str )
     {
-        return std_string< _target_char_type_ >{ reinterpret_cast< const _target_char_type_ * >( _str.data() ) };
+        auto str_it{ reinterpret_cast< const _target_char_type_ * >( _str.cbegin() ) };
+        return std_string< _target_char_type_ >{ str_it, str_it + _str.size() };
     }
     template < typename _target_char_type_, typename _source_char_type_ >
         requires( is_convertible_char_type< _target_char_type_, _source_char_type_ >() )
@@ -172,13 +174,15 @@ namespace cpp_utils {
         requires( is_convertible_char_type< _target_char_type_, _source_char_type_ >() )
     inline auto string_view_convert( const std_string< _source_char_type_ > &_str )
     {
-        return std_string_view< _target_char_type_ >{ reinterpret_cast< const _target_char_type_ * >( _str.c_str() ) };
+        auto str_it{ reinterpret_cast< const _target_char_type_ * >( _str.cbegin() ) };
+        return std_string_view< _target_char_type_ >{ str_it, str_it + _str.size() };
     }
     template < typename _target_char_type_, typename _source_char_type_ >
         requires( is_convertible_char_type< _target_char_type_, _source_char_type_ >() )
     inline auto string_view_convert( const std_string_view< _source_char_type_ > _str )
     {
-        return std_string_view< _target_char_type_ >{ reinterpret_cast< const _target_char_type_ * >( _str.data() ) };
+        auto str_it{ reinterpret_cast< const _target_char_type_ * >( _str.cbegin() ) };
+        return std_string_view< _target_char_type_ >{ str_it, str_it + _str.size() };
     }
     template < typename _target_char_type_, typename _source_char_type_ >
         requires( is_convertible_char_type< _target_char_type_, _source_char_type_ >() )
