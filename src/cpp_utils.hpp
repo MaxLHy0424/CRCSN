0c87f197dc1838bc092133413a00ce92dd192fcc
MaxLHy0424 <MaxLHy974413@outlook.com>
2025-01-12 11:21:33 +0800

    feat(cpp_utils): 添加字符串字面量支持和优化字符串转换

--- src/cpp_utils.hpp
+++ src/cpp_utils.hpp
@@ -119,6 +119,7 @@
 #include <version>
 namespace cpp_utils {
     using namespace std::chrono_literals;
+    using namespace std::string_literals;
     using size_type        = std::size_t;
     using ansi_char        = char;
     using ansi_string      = std::string;
@@ -152,37 +153,61 @@ namespace cpp_utils {
         requires( is_convertible_char_type< _target_char_type_, _source_char_type_ >() )
     inline decltype( auto ) string_convert( const std_string< _source_char_type_ > &_str )
     {
-        return std_string< _target_char_type_ >{ reinterpret_cast< const _target_char_type_ * >( _str.c_str() ) };
+        if constexpr ( std::is_same_v< _target_char_type_, _source_char_type_ > ) {
+            return _str;
+        } else {
+            return std_string< _target_char_type_ >{ reinterpret_cast< const _target_char_type_ * >( _str.c_str() ) };
+        }
     }
     template < typename _target_char_type_, typename _source_char_type_ >
         requires( is_convertible_char_type< _target_char_type_, _source_char_type_ >() )
     inline decltype( auto ) string_convert( const std_string_view< _source_char_type_ > _str )
     {
-        return std_string< _target_char_type_ >{ reinterpret_cast< const _target_char_type_ * >( _str.data() ) };
+        if constexpr ( std::is_same_v< _target_char_type_, _source_char_type_ > ) {
+            return _str;
+        } else {
+            return std_string< _target_char_type_ >{ reinterpret_cast< const _target_char_type_ * >( _str.data() ) };
+        }
     }
     template < typename _target_char_type_, typename _source_char_type_ >
         requires( is_convertible_char_type< _target_char_type_, _source_char_type_ >() )
     inline decltype( auto ) string_convert( const _source_char_type_ *_str )
     {
-        return std_string< _target_char_type_ >{ reinterpret_cast< const _target_char_type_ * >( _str ) };
+        if constexpr ( std::is_same_v< _target_char_type_, _source_char_type_ > ) {
+            return _str;
+        } else {
+            return std_string< _target_char_type_ >{ reinterpret_cast< const _target_char_type_ * >( _str ) };
+        }
     }
     template < typename _target_char_type_, typename _source_char_type_ >
         requires( is_convertible_char_type< _target_char_type_, _source_char_type_ >() )
     inline decltype( auto ) string_view_convert( const std_string< _source_char_type_ > &_str )
     {
-        return std_string_view< _target_char_type_ >{ reinterpret_cast< const _target_char_type_ * >( _str.c_str() ) };
+        if constexpr ( std::is_same_v< _target_char_type_, _source_char_type_ > ) {
+            return _str;
+        } else {
+            return std_string_view< _target_char_type_ >{ reinterpret_cast< const _target_char_type_ * >( _str.c_str() ) };
+        }
     }
     template < typename _target_char_type_, typename _source_char_type_ >
         requires( is_convertible_char_type< _target_char_type_, _source_char_type_ >() )
     inline decltype( auto ) string_view_convert( const std_string_view< _source_char_type_ > _str )
     {
-        return std_string_view< _target_char_type_ >{ reinterpret_cast< const _target_char_type_ * >( _str.data() ) };
+        if constexpr ( std::is_same_v< _target_char_type_, _source_char_type_ > ) {
+            return _str;
+        } else {
+            return std_string_view< _target_char_type_ >{ reinterpret_cast< const _target_char_type_ * >( _str.data() ) };
+        }
     }
     template < typename _target_char_type_, typename _source_char_type_ >
         requires( is_convertible_char_type< _target_char_type_, _source_char_type_ >() )
     inline decltype( auto ) string_view_convert( const _source_char_type_ *_str )
     {
-        return std_string_view< _target_char_type_ >{ reinterpret_cast< const _target_char_type_ * >( _str ) };
+        if constexpr ( std::is_same_v< _target_char_type_, _source_char_type_ > ) {
+            return _str;
+        } else {
+            return std_string_view< _target_char_type_ >{ reinterpret_cast< const _target_char_type_ * >( _str ) };
+        }
     }
     template < typename... _args_ >
     inline auto utf8_format( const utf8_string_view _fmt, _args_ &&..._args )
@@ -213,17 +238,12 @@ namespace cpp_utils {
     {
         std::println( "{}", string_view_convert< ansi_char >( utf8_format( _fmt, std::forward< _args_ >( _args )... ) ) );
     }
-    template < typename _ptr_type_ >
-        requires( std::is_pointer_v< _ptr_type_ > )
-    inline auto ptr_to_ansi_string( _ptr_type_ _ptr )
+    template < typename _char_type_, typename _ptr_type_ >
+        requires( is_char_type< _char_type_ >() && std::is_pointer_v< _ptr_type_ > )
+    inline auto ptr_to_string( _ptr_type_ _ptr )
     {
-        return std::to_string( reinterpret_cast< std::uintptr_t >( _ptr ) );
-    }
-    template < typename _ptr_type_ >
-        requires( std::is_pointer_v< _ptr_type_ > )
-    inline auto ptr_to_utf8_string( _ptr_type_ _ptr )
-    {
-        return string_convert< utf8_char >( std::to_string( reinterpret_cast< std::uintptr_t >( _ptr ) ) );
+        return string_convert< _char_type_ >(
+          _ptr == nullptr ? "nullptr"s : std::format( "0x{:x}", reinterpret_cast< std::uintptr_t >( _ptr ) ) );
     }
     template < typename _char_type_, std::size_t _length_ >
     struct constexpr_string final {
