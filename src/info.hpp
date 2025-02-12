9b8bde520877c626f17729af9c534f96d94401a5
MaxLHy0424 <MaxLHy974413@outlook.com>
2025-02-12 16:07:07 +0800

    v6.1.0-rc.3.0

--- src/info.hpp
+++ src/info.hpp
@@ -1,7 +1,7 @@
 #pragma once
 #define INFO_FULL_NAME  "Computer Room Control Software Nemesis"
 #define INFO_SHORT_NAME "CRCSN"
-#define INFO_VERSION    "v6.1.0-rc.2.0"
+#define INFO_VERSION    "v6.1.0-rc.3.0"
 #define INFO_REPO_URL   "https://github.com/MaxLHy0424/CRCSN"
 #define INFO_DEVELOPER  "MaxLHy0424"
 #define INFO_LICENSE    "MIT License"
